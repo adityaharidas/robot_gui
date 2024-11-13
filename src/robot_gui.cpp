61d0e98ef467044a2bbea01ff28319f2b22d236d
adityaharidas <adityahariofficial@gmail.com>
2024-11-13 17:06:30 +0000

    Displaying the current position of the robot.

--- src/robot_gui.cpp
+++ src/robot_gui.cpp
@@ -3,15 +3,16 @@
 #define CVUI_IMPLEMENTATION
 #include "robot_gui/cvui.h"
 #include "robotinfo_msgs/RobotInfo10Fields.h"
+#include <nav_msgs/Odometry.h> 
 #include <thread>
 #include <geometry_msgs/Twist.h>
 
 class RobotGUI {
 public:
-  RobotGUI() : frame(cv::Mat(600, 800, CV_8UC3)), nh_("~") {
-    // Initialize the subscriber and the GUI window
+  RobotGUI() : frame(cv::Mat(600, 800, CV_8UC3)), nh_("~"), x_(0.0), y_(0.0), z_(0.0) {    // Initialize the subscriber and the GUI window
     info_sub_ =
         nh_.subscribe("/robot_info", 10, &RobotGUI::robotInfoCallback, this);
+        odom_sub_ = nh_.subscribe("/odom", 10, &RobotGUI::odomCallback, this);
     cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
     cvui::init("Robot Info GUI");
     cv::namedWindow("Robot Info GUI");
@@ -30,6 +31,12 @@ public:
     maximum_payload_ = msg->data_field_05;
   }
 
+  void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    x_ = msg->pose.pose.position.x;
+    y_ = msg->pose.pose.position.y;
+    z_ = msg->pose.pose.position.z;
+  }
+
   void run() {
     while (ros::ok()) {
       frame = cv::Scalar(49, 52, 49); // Background color
@@ -42,6 +49,7 @@ public:
       cvui::text(frame, 20, 150, "Firmware Version: " + firmware_version_);
       cvui::text(frame, 20, 180, "Maximum Payload: " + maximum_payload_);
 
+      //Display Teleoperation Controls
       cvui::text(frame, 400, 30, "Teleoperation Control");
       if (cvui::button(frame, 400, 60, "Forward")) {
         linear_speed_ = 0.5; // Move forward with positive speed
@@ -68,11 +76,17 @@ public:
         angular_speed_ = 0.0;
         publishVelocities();
             }
-    // displaying current velocities
+    // Display current velocities
       cvui::text(frame, 400, 260, "Current Velocities");
       cvui::text(frame, 400, 290, "Linear Velocity (X): " + std::to_string(linear_speed_));
       cvui::text(frame, 400, 320, "Angular Velocity (Z): " + std::to_string(angular_speed_));
     
+      //Display Current Position
+      cvui::text(frame, 20, 220, "Robot Position (Odometry)");
+      cvui::text(frame, 20, 250, "X: " + std::to_string(x_));
+      cvui::text(frame, 20, 280, "Y: " + std::to_string(y_));
+      cvui::text(frame, 20, 310, "Z: " + std::to_string(z_));
+
       cvui::update();
       cv::imshow("Robot Info GUI", frame);
       if (cv::waitKey(20) == 27)
@@ -82,7 +96,7 @@ public:
 
 private:
   ros::NodeHandle nh_;
-  ros::Subscriber info_sub_;
+  ros::Subscriber info_sub_, odom_sub_;
   cv::Mat frame;
   std::thread ros_thread_;
   ros::Publisher cmd_vel_pub_;
@@ -90,7 +104,8 @@ private:
 
   std::string robot_description_, serial_number_, ip_address_,
       firmware_version_, maximum_payload_;
-
+  double x_, y_, z_;
+  
   void publishVelocities() {
         geometry_msgs::Twist vel_msg;
         vel_msg.linear.x = linear_speed_;
